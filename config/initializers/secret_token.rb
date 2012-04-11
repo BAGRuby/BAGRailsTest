9939f1c2eb444ff13adb9bfbc8ea44b925711c4e
BAG <adam.webster@bluearcgroup.com>
2012-04-11 11:41:21 +1000

    initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BAGRailsTest::Application.config.secret_token = '93a56fb380d0d27de02d2c2d4e14043af92ea11aa7c36ec984f492cfbdc7281e80438b879efe6fa2bf90148b4d536d1e9454ab57da58cff76235463ff82a3c12'
